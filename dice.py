ce5aa9dd4f11876a3a1535207769dc8f59a8ecf0
soyasauce06 <siya_patel@college.harvard.edu>
2026-05-04 00:09:41 -0400

    Update dice.py

--- dice.py
+++ dice.py
@@ -1,17 +1,18 @@
-# Dice 1 will have a 50% chance of removing your opponents piece and a 50% chance you have to remove your own piece. 
-# Dice 1 will have two functions. A function for removing your piece and a function for removing your opps piece. 
-# Dice 2 will have a 25% chance of removing your opponents piece and a 25% chance of removing your own piece. This means, 
+# Dice 1 will have a 50% chance of replacing your opponents piece and a 50% chance you lose your turn. 
+# Dice 2 will have a 25% chance of replacing your opponents piece and a 25% chance of losing your turn. This means, 
 # there is also a 50% chance that your dice rolls nothing siginificant, and should this happen, you are then prompted to 
 # place down a piece as usual. 
-# Dice 3 will have a 1/6 chance of removing your opponents piece and a 1/6 chance of removing your own piece. This means, 
+# Dice 3 will have a 1/6 chance of replacing your opponents piece and a 1/6 chance of losing your turn. This means, 
 # there is a 4/6 chance that your dice rolls nothing significant, and should this happen, you are then prompted to place 
 # down a piece as usual. 
 # If you do roll something of significance, after carrying out the dice's task, your turn will end. Meaning, should you get to 
-# remove your opponent's piece or have to remove your own piece, your turn will end after, and you will not get the chance to then
+# replace your opponent's piece or lose your turn, your turn will end after, and you will not get the chance to then
 # place down a piece. 
+
 import random 
-# make a function that prompts you to remove a piece. make sure you are only removing a piece for the available board space. 
-def remove_piece(board, target_marker): 
+
+# make a function that prompts you to replace a piece. make sure you are only replacing a piece for the available board space. 
+def replace_piece(board, target_marker, replacement_marker): 
   piece_exists = False 
   for row in board: 
     if target_marker in row: 
@@ -19,12 +20,12 @@ def remove_piece(board, target_marker):
       break 
       
   if not piece_exists: 
-    print(f" No '{target_marker}' pieces on the board to remove! ")
+    print(f" No '{target_marker}' pieces on the board to replace! ")
     return False
     
   while True: 
     try: 
-      move = int(input(f" Select a ' {target_marker} ' piece to remove (1-9): "))
+      move = int(input(f" Select a ' {target_marker} ' piece to replace (1-9): "))
       if move < 1 or move > 9:
         print("Please enter a number between 1-9.")
         continue
@@ -37,56 +38,54 @@ def remove_piece(board, target_marker):
         print(f"  That spot does not contain a '{target_marker}'. Try again...")
         continue
             
-      # Remove the piece by replacing it with a blank space
-      board[row][col] = " "
-      print(f"  '{target_marker}' piece removed!")
+      # Replace the opponent's piece with the current player's marker
+      board[row][col] = replacement_marker
+      print(f"  '{target_marker}' piece replaced with '{replacement_marker}'!")
       return True
       
     except ValueError: 
       print("Numbers outside of 1-9 are invalid. Pick again...")
+
 
 def roll_dice_one(board, current_player, opponent):
   roll = random.randint(1,6)
   print(f"\n  🎲 You rolled a {roll}!") 
 
   if roll in [1,2,3]: 
-    print(f"  Awesome! You get to remove one of Player {opponent}'s pieces.")
-    return remove_piece(board, opponent) 
+    print(f"  Awesome! You get to replace one of Player {opponent}'s pieces with your own.")
+    return replace_piece(board, opponent, current_player) 
   else: # roll is 4,5,6
-    print(f"  Oh no! You rolled too high. You must remove one of your own '{current_player}' pieces.")
-    return remove_piece(board, current_player)
+    print("  OH NO, you rolled too high. TURN SKIPPED WOMP WOMP")
+    return True # Returning True skips the placement phase
 
 
-# dice 2 will have a 1/4 chance for removing opps or your own piece. 1/2 chance for null and placing a piece like normal game play
-
+# dice 2 will have a 1/4 chance for replacing opps piece or losing a turn. 1/2 chance for null and placing a piece like normal game play
 def roll_dice_two(board, current_player, opponent):
   roll = random.randint(1,4)
   print(f"\n  🎲 You rolled a {roll}!") 
 
   if roll == 1:
-    print(f"  You got a 1! You get to remove one of Player {opponent}'s pieces.")
-    return remove_piece(board, opponent)  
+    print(f"  You got a 1! You get to replace one of Player {opponent}'s pieces with your own.")
+    return replace_piece(board, opponent, current_player)  
   elif roll == 4:
-    print(f"  Oh no! You rolled a 4. You must remove one of your own '{current_player}' pieces.")
-    return remove_piece(board, current_player)
+    print("  OH NO, you rolled a 4. TURN SKIPPED WOMP WOMP")
+    return True
   else: # if roll in 2 or 3 
     print(f" No special moves! Place down a piece like regular-smegular.")
     return False
 
-# dice 3 will have a 1/6 chance for removing opps or your own piece. 4/6 chance for null and placing a piece like normal game play
 
+# dice 3 will have a 1/6 chance for replacing opps piece or losing a turn. 4/6 chance for null and placing a piece like normal game play
 def roll_dice_three(board, current_player, opponent):
   roll = random.randint(1,6)
-  print(f"\n You rolled a {roll}!")
+  print(f"\n  🎲 You rolled a {roll}!")
 
   if roll == 1: 
-    print(f" Awesome! You get to remove one of Player {opponent}'s pieces.")
-    return remove_piece(board, opponent)
+    print(f"  Awesome! You get to replace one of Player {opponent}'s pieces with your own.")
+    return replace_piece(board, opponent, current_player)
   elif roll == 6: 
-    print(f" Oh no! You rolled too high. You must remove one of your own '{current_player}' pieces.")
-    return remove_piece(board, current_player) 
+    print("  OH NO, you rolled too high. TURN SKIPPED WOMP WOMP")
+    return True 
   else: 
-    print(" Nothing significant happened! You must place down a piece...")
-    return False 
-
-  
+    print("  Nothing significant happened! You must place down a piece...")
+    return False
